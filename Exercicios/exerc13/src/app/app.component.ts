23718d2bb2f7a825c8fef50686e9c5ad8e40191a
Denis Souza <denis.souza.rosa@gmail.com>
2019-10-02 12:09:25 -0300

    Finalizado vinculação no Model

--- Exercicios/exerc13/src/app/app.component.ts
+++ Exercicios/exerc13/src/app/app.component.ts
@@ -16,19 +16,9 @@ export class AppComponent {
 
   orderModel = new Order('', '', 0, '', '', true);
 
+  
 
-
-  AtualizaModel(dados){
-
-    //console.log(dados.value.drink);
-
-    this.orderModel = new Order(
-      dados.value.name,
-      dados.value.email,
-      dados.value.phone,
-      dados.value.drink,
-      dados.value.tempPreference, true);
-
-  }
+  
+  
 
 }
